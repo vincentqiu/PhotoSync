bbf61d0636f2b1f92e233a533f50835b6cecc7b8
vincentqiu <vincent.qiu21@gmail.com>
2014-05-22 18:56:01 +0800

    sdk plugin

--- app/src/main/java/com/photosync/app/MainActivity.java
+++ app/src/main/java/com/photosync/app/MainActivity.java
@@ -1,30 +1,116 @@
 package com.photosync.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.StringRes;
 
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
-import java.util.Date;
+import java.util.HashMap;
+
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.qiniu.auth.JSONObjectRet;
+import com.qiniu.io.IO;
+import com.qiniu.io.PutExtra;
 
-public class MainActivity extends ActionBarActivity {
+
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity  {
+
+    public static final int PICK_PICTURE_RESUMABLE = 0;
+
+    // @gist upload_arg
+    public static String bucketName = "<bucketName>";
+    public static String domain = bucketName + ".qiniudn.com";
+    // upToken 这里需要自行获取. SDK 将不实现获取过程. 当token过期后才再获取一遍
+    public String uptoken = "<token>";
+    // @endgist
+
+
+    @ViewById
+    TextView hint;
+
+    @ViewById
+    Button btnUpload;
+
+    @ViewById
+    Button  btnResumableUpload;
+
+
+    /**
+     * 初始化控件
+     */
+    private void initWidget() {
+/*        btnUpload.setOnClickListener(this);
+        btnResumableUpload.setOnClickListener(this);*/
+    }
+
+    // @gist upload
+    boolean uploading = false;
+    /**
+     * 普通上传文件
+     * @param uri
+     */
+    private void doUpload(Uri uri) {
+        if (uploading) {
+            hint.setText("上传中，请稍后");
+            return;
+        }
+        uploading = true;
+        String key = IO.UNDEFINED_KEY; // 自动生成key
+        PutExtra extra = new PutExtra();
+        extra.params = new HashMap<String, String>();
+        extra.params.put("x:a", "测试中文信息");
+        hint.setText("上传中");
+        IO.putFile(this, uptoken, key, uri, extra, new JSONObjectRet() {
+            @Override
+            public void onProcess(long current, long total) {
+                hint.setText(current + "/" + total);
+            }
+
+            @Override
+            public void onSuccess(JSONObject resp) {
+                uploading = false;
+                String hash = resp.optString("hash", "");
+                String value = resp.optString("x:a", "");
+                String redirect = "http://" + domain + "/" + hash;
+                hint.setText("上传成功! " + hash);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(redirect));
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(Exception ex) {
+                uploading = false;
+                hint.setText("错误: " + ex.getMessage());
+            }
+        });
+    }
+    // @endgist
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.err.println("app create");
+        initWidget();
+        System.err.println("event bind");
+
     }
 
 
@@ -46,4 +132,42 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Click(R.id.btnUpload)
+    void btnUploadClicked() {
+        System.err.println("click trigger!");
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, PICK_PICTURE_RESUMABLE);
+        return;
+    }
+
+    @Click(R.id.btnResumableUpload)
+    void btnResumableUploadClicked(View v){
+        startActivity(new Intent(this, MyResumableActivity_.class));
+    }
+
+/*    @Override
+    public void onClick(View view) {
+        System.err.println("click trigger!");
+        if (view.equals(btnUpload)) {
+            Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, PICK_PICTURE_RESUMABLE);
+            return;
+        }
+        if (view.equals(btnResumableUpload)) {
+            //startActivity(new Intent(this, MyResumableActivity.class));
+            return;
+        }
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+        if (requestCode == PICK_PICTURE_RESUMABLE) {
+            doUpload(data.getData());
+            return;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/photosync/app/MyResumableActivity.java
@@ -0,0 +1,166 @@
+package com.photosync.app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.qiniu.auth.JSONObjectRet;
+import com.qiniu.resumableio.PutExtra;
+import com.qiniu.resumableio.ResumableIO;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.StringRes;
+import org.w3c.dom.Text;
+
+
+@EActivity(R.layout.resumable)
+public class MyResumableActivity extends ActionBarActivity {
+
+    int taskId = -1;
+    Uri uploadUri;
+    PutExtra mExtra;
+
+    @ViewById
+    Button  start;
+    @ViewById
+    Button  stop;
+    @ViewById
+    TextView    hint;
+    @ViewById
+    ProgressBar pb;
+
+    @Click(R.id.start)
+    void startClicked() {
+         selectFile();
+    }
+
+    @Click(R.id.stop)
+    void stopClicked() {
+        if (uploadUri == null) {
+            Toast.makeText(this, "还没开始任务", 20).show();
+            return;
+        }
+        if (taskId >= 0) {
+            ResumableIO.stop(taskId);
+            stop.setText("开始");
+            hint.setText("暂停");
+            taskId = -1;
+            return;
+        }
+        stop.setText("暂停");
+        hint.setText("连接中");
+        doResumableUpload(uploadUri, mExtra);
+        return;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resumable);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_resumable, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void doResumableUpload(final Uri uri, PutExtra extra) {
+        hint.setText("连接中");
+        String key = null;
+        String token = "<token>";
+        extra.params = new HashMap<String, String>();
+        extra.params.put("x:a", "bb");
+        taskId = ResumableIO.putFile(this, token, key, uri, extra, new JSONObjectRet() {
+            @Override
+            public void onSuccess(JSONObject obj) {
+                hint.setText("上传成功: " + obj.optString("key", ""));
+            }
+
+            @Override
+            public void onProcess(long current, long total) {
+                float percent = (float) (current*10000/total) / 100;
+                hint.setText("上传中: " + percent + "%");
+                pb.setProgress((int) percent);
+            }
+
+            @Override
+            public void onPause(Object tag) {
+                uploadUri = uri;
+                mExtra = (PutExtra) tag;
+            }
+
+            @Override
+            public void onFailure(Exception ex) {
+                hint.setText(ex.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+        PutExtra e = new PutExtra();
+        e.notify = new PutExtra.INotify() {
+            @Override
+            public void onSuccessUpload(PutExtra ex) {
+                if (ex.isFinishAll()) return;
+                JSONObject json;
+                try {
+                    json = ex.toJSON();
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
+                    return;
+                }
+                // store to disk
+                // restore PutExtra by new PutExtra(JSONObject);
+            }
+        };
+        doResumableUpload(data.getData(), e);
+    }
+
+    public void selectFile() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "请选择一个要上传的文件"), 1);
+        } catch (android.content.ActivityNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+}
